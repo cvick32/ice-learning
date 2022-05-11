79f4ba9548371512239862353300312dd1f51aa5
cole <cole.d.vick@gmail.com>
2022-05-10 20:38:13 -0500

    add files

---
+++ ice.py
@@ -0,0 +1,299 @@
+from z3 import Bool, Implies, And, Or, Not, Int, Solver, IntNumRef, Array, IntSort
+from collections import defaultdict
+'''
+ICE Learning example:
+
+
+given a few preconditions, want to try to synthesize an invariant
+
+here are the preconditions  (i guess we can think of this as initial state):
+
+p >= 25
+p < 75
+a[p] == 1
+i = 0
+j = 0
+
+post condition:
+
+j == 1
+
+trans relation (loop):
+if a[i] == 1 -> j = 1
+else i++, a == a, j == j
+'''
+
+class Teacher:
+
+    def __init__(self, all_vars, pre_condition, post_condition):
+        pass
+
+    def check_invariant(self, inv):
+        pass
+
+i, j, p, a = Int('i'), Int('j'), Int('p'), Array('a', IntSort(), IntSort())
+Teacher(['i', 'j', 'p', 'a'], [i >= 25, p < 75, a[p] == 1, i == 0, j == 0], j == 1)
+
+class Learner:
+    def __init__(self, all_vars, num_disjuncts, num_conjuncts, upper_bound):
+        self.all_vars = all_vars
+        self.num_vars = len(self.all_vars)
+        self.num_to_var_str = self.num_to_var_str_gen()
+
+        self.num_disjuncts = num_disjuncts # i in the paper
+        self.num_conjuncts = num_conjuncts # j in the paper
+        self.upper_bound = upper_bound
+
+        self.examples = []
+        self.counterexamples = []
+        self.implication_pairs = []
+
+        self.datapoint_bools = []
+        self.solving_for = defaultdict(set)
+
+    def add_example(self, ex):
+        self.examples.append(ex)
+
+    def add_counterexample(self, cex):
+        self.counterexamples.append(cex)
+
+    def add_implication_pair(self, i1, i2):
+        self.implication_pairs.append((i1, i2))
+
+    def run_solver(self):
+        form = l.construct_full_smt_formula()
+        s = Solver()
+        s.add(form)
+        if str(s.check()) == 'sat':
+            self.cur_model = s.model()
+            # d = {}
+            # for i in self.cur_model:
+            #     d[str(i)] = self.cur_model[i]
+            # for i in sorted(d.keys()):
+            #     print(f"{i}: {d[i]}")
+            return self.get_invariant_structure()
+        else:
+            # paper says 'dovetail' between the 2 not sure how to do that
+            print('have to change c or change disjunct, conjunct structure')
+
+    def get_invariant_structure(self):
+        disjuncts = []
+        for d in range(self.num_disjuncts):
+            conjuncts = []
+            for c in range(self.num_conjuncts):
+                s = f"d{d}_c{c}"
+                var_dict = {}
+                for i in self.solving_for[s]:
+                    print(i)
+                    print(d)
+                    v = self.cur_model[i]
+                    if 's1' in str(i):
+                        var_dict['s1'] = v
+                    elif 's2' in str(i):
+                        var_dict['s2'] = v
+                    elif 'v1' in str(i):
+                        var_dict['v1'] = self.num_to_z3_var(v)
+                    elif 'v2' in str(i):
+                        var_dict['v2'] = self.num_to_z3_var(v)
+                    elif 'c_' in str(i):
+                        var_dict['c'] = v
+                    else:
+                        raise ValueError(f'invalid `solving for` key: {i}')
+                print(f'here: {var_dict}')
+                conjuncts.append((var_dict['s1'] * var_dict['v1']) + (var_dict['s2'] * var_dict['v2']) <= var_dict['c']) # fundamental structure
+            disjuncts.append(And(conjuncts))
+        return Or(disjuncts)
+
+
+    def num_to_var_str_gen(self):
+        d = {}
+        for i, var_str in enumerate(self.all_vars):
+            d[i] = var_str
+        return d
+
+    def num_to_z3_var(self, num):
+        if isinstance(num, IntNumRef):
+            num = int(str(num))
+        return Int(self.num_to_var_str[num])
+
+    def construct_full_smt_formula(self):
+        formula = []
+        formula.append(self.conjuncts_for_examples())
+        formula.append(self.conjuncts_for_cex())
+        formula.append(self.conjuncts_for_implications())
+        formula.append(self.conjuncts_for_dps())
+        formula.append(self.generate_bij_equality())
+        formula.append(self.generate_all_c_in_bounds())
+        formula.append(self.generate_multiplication())
+        formula.append(self.generate_var_values_with_datapoints())
+        formula.append(self.generate_multiplier_values())
+        formula.append(self.generate_var_nums())
+        formula = And(formula)
+        return formula
+
+    def conjuncts_for_examples(self):
+        # first on line 1
+        ex_bools = []
+        for i, dp in enumerate(self.examples):
+            var_dict = dp
+            ex_bool = Bool(f'example_{i}')
+            self.datapoint_bools.append([ex_bool, var_dict])
+            ex_bools.append(ex_bool)
+        return And(ex_bools)
+
+    def conjuncts_for_cex(self):
+        # second on line 1
+        cex_bools = []
+        for i, dp in enumerate(self.counterexamples):
+            var_dict = dp
+            cex_bool = Bool(f'cex_{i}')
+            cex_bools.append(Not(cex_bool))
+            self.datapoint_bools.append([cex_bool, var_dict])
+        return And(cex_bools)
+
+    def conjuncts_for_implications(self):
+        # third on line 1
+        imp_bools = []
+        for i, dp in enumerate(self.implication_pairs):
+            var_dict_lhs = dp[0]
+            var_dict_rhs = dp[1]
+            imp_bool_lhs = Bool(f'impl_{i}_lhs')
+            imp_bool_rhs = Bool(f'impl_{i}_rhs')
+            self.datapoint_bools.append([imp_bool_lhs, var_dict_lhs])
+            self.datapoint_bools.append([imp_bool_rhs, var_dict_rhs])
+            imp_bool = Implies(imp_bool_lhs, imp_bool_rhs)
+            imp_bools.append(imp_bool)
+        return And(imp_bools)
+
+    def conjuncts_for_dps(self):
+        # last on line 1
+        conjuncts = []
+        for i, dp in enumerate(self.datapoint_bools):
+            disjuncts = []
+            for d in range(self.num_disjuncts):
+                inner_conjuncts = []
+                for c in range(self.num_conjuncts):
+                    b = Bool(f"b_p{i}_c{c}_d{d}")
+                    inner_conjuncts.append(b)
+                disjuncts.append(And(inner_conjuncts))
+            conjuncts.append(dp[0] == Or(disjuncts))
+        return And(conjuncts)
+
+    def generate_bij_equality(self): # change to include all DPs
+        # first on line 2
+        equalities = []
+        for i, dp in enumerate(self.datapoint_bools):
+            for d in range(self.num_disjuncts):
+                for conj in range(self.num_conjuncts):
+                    b = Bool(f"b_p{i}_d{d}_c{conj}")
+                    c = Int(f"c_d{d}_c{conj}")
+                    r1 = Int(f"r1_p{i}_d{d}_c{conj}")
+                    r2 = Int(f"r2_p{i}_d{d}_c{conj}")
+                    equality = b == ((r1 + r2) <= c)
+                    equalities.append(equality)
+        return And(equalities)
+
+    def generate_all_c_in_bounds(self):
+        # middle of line 2
+        constraints = []
+        for d in range(self.num_disjuncts):
+            for nc in range(self.num_conjuncts):
+                c = Int(f"c_d{d}_c{nc}")
+                b1 = c >= -self.upper_bound # -M
+                b2 = c <= self.upper_bound # M
+                constraint = And(b1, b2)
+                constraints.append(constraint)
+                self.solving_for[f"d{d}_c{nc}"].add(c)
+        return And(constraints)
+
+    def generate_multiplication(self):
+        # last on line 2
+        conjuncts = []
+        for i, dp in enumerate(self.datapoint_bools):
+            for nd in range(self.num_disjuncts):
+                for c in range(self.num_conjuncts):
+                    for k in range(1, 3):
+                        r = Int(f"r{k}_p{i}_d{nd}_c{c}")
+                        d = Int(f"d{k}_p{i}_d{nd}_c{c}")
+                        s = Int(f"s{k}_d{nd}_c{c}")
+                        b1 = Implies(s == 0, r == 0)
+                        b2 = Implies(s == 1,  r == d)
+                        b3 = Implies(s == -1, r == -d)
+                        conjunct = And(b1, b2, b3)     # This might not be correct, the paper doesn't specify which operator to use
+                        conjuncts.append(conjunct)
+                        self.solving_for[f"d{nd}_c{c}"].add(s)
+        return And(conjuncts)
+
+
+    def generate_var_values_with_datapoints(self):
+        # first on line 3
+        conjuncts = []
+        for i, dp in enumerate(self.datapoint_bools):
+            var_dict = dp[1]
+            for nd in range(self.num_disjuncts):
+                for c in range(self.num_conjuncts):
+                    for k in range(1, 3):
+                        v = Int(f"v{k}_d{nd}_c{c}")
+                        self.solving_for[f"d{nd}_c{c}"].add(v)
+                        d = Int(f"d{k}_p{i}_d{nd}_c{c}")
+                        for l in range(self.num_vars):
+                            conjuncts.append(Implies(v == l, d == var_dict[self.num_to_var_str[l]]))
+        return And(conjuncts)
+
+    def generate_multiplier_values(self):
+        # second on line 3
+        conjuncts = []
+        for d in range(self.num_disjuncts):
+            for c in range(self.num_conjuncts):
+                for k in range(1, 3):
+                    s = Int(f"s{k}_d{d}_c{c}")
+                    b1 = s >= -1
+                    b2 = s <= 1
+                    conjunct = And(b1, b2)
+                    conjuncts.append(conjunct)
+        return And(conjuncts)
+
+    def generate_var_nums(self):
+        # third and last on line 3
+        conjuncts = []
+        for d in range(self.num_disjuncts):
+            for c in range(self.num_conjuncts):
+                for k in range(1, 3):
+                    v = Int(f"v{k}_d{d}_c{c}")
+                    b1 = v >= 0
+                    b2 = v <= self.num_vars - 1 # n in paper
+                    conjunct = And(b1, b2)
+                    conjuncts.append(conjunct)
+                v1 = Int(f"v1_d{d}_c{c}")
+                v2 = Int(f"v2_d{d}_c{c}")
+                not_equal = v1 != v2
+                conjuncts.append(not_equal)
+        return And(conjuncts)
+
+
+num_disjuncts = 1
+num_conjuncts = 1
+upper_bound = 100
+l = Learner(['i', 'j', 'p'], num_disjuncts, num_conjuncts, upper_bound)
+
+l.add_example({'i': 0, 'j': 0, 'p': 25})
+l.add_example({'i': 1, 'j': 0, 'p': 25})
+l.add_example({'i': 1, 'j': 1, 'p': 25})
+l.add_example({'i': 25, 'j': 25, 'p': 25})
+l.add_example({'i': 26, 'j': 1, 'p': 25})
+l.add_example({'i': 50, 'j': 51, 'p': 51})
+l.add_example({'i': 53, 'j': 1, 'p': 51})
+l.add_example({'i': 54, 'j': 1, 'p': 51})
+l.add_example({'i': 55, 'j': 1, 'p': 51})
+
+l.add_counterexample({'i': 100, 'j': 0, 'p': 25})
+l.add_counterexample({'i': 100, 'j': 2, 'p': 25})
+l.add_counterexample({'i': 98, 'j': 5, 'p': 25})
+l.add_counterexample({'i': 99, 'j': 0, 'p': 25})
+l.add_counterexample({'i': 99, 'j': 2, 'p': 25})
+
+
+l.add_implication_pair({'i': 50, 'j': 0, 'p': 25}, {'i': 51, 'j': 0, 'p': 25})
+l.run_solver()
+
+
---
+++ weakest-pre.py
@@ -0,0 +1,184 @@
+from z3 import *
+import inspect
+
+def set_(v, e):
+    return lambda post: substitute(post, (v, e)) # set
+
+def seq(s1, s2):
+    return lambda post: s1(s2(post)) # se1
+
+def begin(*program_statements):
+    def begin_res(post):
+        wp = post
+        for statement in reversed(program_statements):
+            print(f"arg: {inspect.getsource(statement)}")
+            wp = statement(wp)
+            print(f"new weakest precondition: {wp}")
+        return wp
+    return begin_res
+
+def if_(cond, t, e):
+    return lambda post: Or(Implies(cond, t(post)), And(Not(cond), e(post))) # if
+
+skip = lambda post: post
+abort = lambda post: BoolVal(False)
+
+def assert_(prop):
+    return lambda post: And(post, prop)
+
+def assume_(prop):
+    return lambda post: Implies(prop, post)
+
+def is_var(expr):
+    # it's a var if it has no children and is alpha
+    return str(expr).isalpha()
+
+def get_vars_help(expr, cur_vars):
+    if expr.children():
+        for child in expr.children():
+            get_vars_help(child, cur_vars)
+    else:
+        if is_var(expr):
+            cur_vars.append(expr)
+
+def get_vars(expr):
+    vs = []
+    get_vars_help(expr, vs)
+    return vs
+
+
+def while_(cond, inv, *body):
+    def while_res(post):
+        vs = list(get_vars(post))
+        return And(inv, ForAll(vs, And(Implies(And(cond, inv), begin(*body)(inv)),
+                                       Implies(And(Not(cond), inv), post))))
+    return while_res
+
+def debug(post):
+    print(post)
+    return post
+
+def verify_fun(pre, post, body):
+    prove(Implies(pre, body(post)))
+
+I = IntSort()
+
+# example from paper
+
+i, j, p = Ints('i j p')
+a = Array('a', I, I)
+ice_example = begin(set_(i, IntVal(0)),
+                    set_(j, IntVal(0)),
+                    while_(i < 100, j != j,
+                           if_(Select(a, i) == 1, set_(j, IntVal(1)), set_(j, j)),
+                           set_(i, i + 1)))
+
+verify_fun(And(p >= 25, p < 75, Select(a, p) == 1), j == 1, ice_example)
+
+# j, k, menor, N = Ints('j k menor N')
+# arr = Array('arr', IntSort(), IntSort())
+# array_safe = \
+#     begin(set_(N, IntVal(1)),
+#           set_(j, IntVal(0)),
+#           while_(j < N, j == 0,
+#                  set_(arr, Store(arr, j, k)),
+#                  if_(Select(arr, j) <= menor,
+#                      set_(menor, Select(arr, j)),
+#                      set_(menor, menor))))
+
+# verify_fun(True, Select(arr, 0) >= menor, array_safe)
+
+# term 2
+
+# x, y, z = Ints('x y z')
+# temp = Bool('temp')
+
+# term2 = begin(while_(And(x < 100, z < 100), x == x,
+#                      if_(temp, set_(x, x + 1), set_(x, x - 1)),
+#                      if_(temp, set_(x, x + 1), set_(z, z - 1))))
+
+# verify_fun(True, Or(x >= 100, z <= 100), term2)
+
+
+# sn, loop1, n1, x = Ints('sn loop1 n1 x')
+
+# sum3 = begin(set_(sn, IntVal(0)),
+#              while_(True
+
+
+# small example
+# i, N, Z = Ints('i N Z')
+# a = Array('a', I, I)
+# b = Array('b', I, I)
+# write_bool = Bool('write_bool')
+# test1 = begin(set_(a, K(IntSort(), 0)),
+#               set_(b, K(IntSort(), 1)),
+#               set_(i, IntVal(0)),
+#               while_(i < N, i == i,
+#                      if_(write_bool, set_(b, Store(b, Select(a, i), i)), set_(b, b)),
+#                      set_(i, i + 1)))
+
+# verify_fun(And(Z >= 0, Z <= N, N > 0), Select(b, Z) <= 1, test1)
+
+
+
+
+
+
+# def twosort(a, b):
+#     if b < a:
+#         temp = a
+#         a = b
+#         b = temp
+#     else:
+#         pass
+#     return a, b
+
+# a, b, c, d, temp = Ints("a b c d temp")
+# prog1 = \
+#     if_(b < a,
+#         begin(
+#             set_(temp, a),
+#             set_(a, b),
+#             set_(b, temp)),
+#         skip)
+
+
+# prog2 = \
+#     begin(
+#         set_(c, d - c),
+#         set_(d, d - c),
+#         set_(c, c + d))
+
+
+# def square(n):
+#     a = 0
+#     i = 0
+#     while i < n:
+#         assert a == n * i
+#         i = i + 1
+#         a = a + n
+#     return a
+
+# i, n = Ints('i n')
+# sq = begin(
+#     set_(a, IntVal(0)),
+#     set_(i, IntVal(0)),
+#     while_(i < n, And(i < (n + 1), 0 <= i, a == n * i),
+#            set_(i, i + 1),
+#            set_(a, a + n)))
+
+# #verify_fun(n >= 0, a == n * n, sq)
+
+
+
+# sumn = Function("sumn", IntSort(), IntSort())
+# sumndef = ForAll([a], sumn(a) == If(a <= 0, 0, sumn(a - 1) + a))
+
+# sumn_prog = \
+#     begin(set_(i, IntVal(0)),
+#           while_(i != n + 1, a == sumn(i),
+#                  set_(i, i + 1),
+#                  set_(a, a + 1)))
+
+# verify_fun(And(sumndef, n >= 0), sumn(n) == a, sumn_prog)
